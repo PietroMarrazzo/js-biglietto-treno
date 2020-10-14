892de433e7297d7fca1e14c5e05d064c545f9f64
Pietro <marrazzo.pietroalfonso@gmail.com>
2020-10-14 15:31:40 +0200

    job done?

--- js/main.js
+++ js/main.js
@@ -1,15 +1,38 @@
 // richieste
-var annoNascita=prompt("Inserisci la tua data di nascita");
-var percorrenza=.parseInt(prompt("inserisci la distanza che vuoi percorrere in Kilometri"));
+var annoNascita = prompt("Inserisci la tua data di nascita");
+var percorrenza = parseInt(prompt("inserisci la distanza che vuoi percorrere in Kilometri"));
 
+// variabili
 // prezzo biglietto
 var prezzoKm=0.21;
 
 // anno attuale
-var annoAttuale=now Date();
+var annoAttuale= new Date();
+
+//costanti
+// under 18
+var under=18;
+//sconto under
+var scontoUnder= 0.2;
+// senior
+var senior=64;
+var scontoSenior= 0.4;
 
 // età passeggero
-var etaPasseggero=annoAttuale - annoNascita;
+var etaPasseggero= annoAttuale - annoNascita;
 
 // prezzo standard Biglietto
-var prezzoBase= prezzoKm * percorrenza;
+var prezzo= prezzoKm * percorrenza;
+
+//condizione di sconto
+if (etaPasseggero < under) {
+  var sconto = prezzo * scontoUnder;
+  var prezzo = prezzo - sconto;
+}
+else if (etaPasseggero > senior) {
+  var sconto = prezzo * scontoSenior;
+  var prezzo = prezzo - sconto;
+}
+
+//visualizzazione
+document.getElementById("prezzo").innerHTML= prezzo + " " + "euro";
