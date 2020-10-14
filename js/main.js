9abcfd466a919ceb3775a3d064cdb8befdcc9ad1
Pietro <marrazzo.pietroalfonso@gmail.com>
2020-10-14 15:46:35 +0200

    fixing

--- js/main.js
+++ js/main.js
@@ -1,38 +1,48 @@
 // richieste
-var annoNascita = prompt("Inserisci la tua data di nascita");
+var annoNascita = parseInt(prompt("Inserisci la tua data di nascita"));
 var percorrenza = parseInt(prompt("inserisci la distanza che vuoi percorrere in Kilometri"));
 
 // variabili
 // prezzo biglietto
-var prezzoKm=0.21;
+var prezzoKm = 0.21;
 
 // anno attuale
-var annoAttuale= new Date();
+var annoAttuale = new Date();
 
 //costanti
 // under 18
-var under=18;
+var under = 18;
 //sconto under
-var scontoUnder= 0.2;
+var scontoUnder = 0.2;
 // senior
-var senior=64;
-var scontoSenior= 0.4;
+var senior = 4;
+var scontoSenior = 0.4;
 
 // età passeggero
-var etaPasseggero= annoAttuale - annoNascita;
+var etaPasseggero = annoAttuale - annoNascita;
 
 // prezzo standard Biglietto
-var prezzo= prezzoKm * percorrenza;
+var prezzo = prezzoKm * percorrenza;
 
 //condizione di sconto
 if (etaPasseggero < under) {
   var sconto = prezzo * scontoUnder;
   var prezzo = prezzo - sconto;
+  //visualizzazione
+  document.getElementById("prezzo").innerHTML= prezzo + " " + "euro";
+} else {
+  // //visualizzazione
+  // document.getElementById("prezzo").innerHTML= prezzo + " " + "euro";
 }
-else if (etaPasseggero > senior) {
-  var sconto = prezzo * scontoSenior;
-  var prezzo = prezzo - sconto;
-}
+// else if (etaPasseggero > senior) {
+//   var sconto = prezzo * scontoSenior;
+//   var prezzo = prezzo - sconto;
+//   //visualizzazione
+//   document.getElementById("prezzo").innerHTML= prezzo + " " + "euro";
+// } else {
+//   //visualizzazione
+//   document.getElementById("prezzo").innerHTML= prezzo + " " + "euro";
+// }
 
-//visualizzazione
-document.getElementById("prezzo").innerHTML= prezzo + " " + "euro";
+// //visualizzazione
+// document.getElementById("prezzo").innerHTML= prezzo + " " + "euro";
